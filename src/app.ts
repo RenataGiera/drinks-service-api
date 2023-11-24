152403f47e6cb22c608df1cc1b665682d5d6ec32
Renata Giera-Dubiel <renatagiera@gmail.com>
2023-11-24 20:34:50 +0000

    refactor: updated file name for  GET request for /coffee  endpoint and test

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { router } from './routers/routes';
+
 export const app = express ();
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('Welcome to the Drinks API'));
 app.get('/coffeelover', (req, res) => res.send('I like coffee!'));
+app.use('/', router);
