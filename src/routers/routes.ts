725ae7adec82e3d0115c416817156f5e3efcbe89
Renata Giera-Dubiel <renatagiera@gmail.com>
2023-11-26 03:20:39 +0000

    feat:add a Full Server

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import * as coffeeController from '../controllers/coffee_controller';
+import {coffeeLoverController} from '../controllers/coffeelover_controller';
+import * as teaController from '../controllers/tea_controller';
 
 export const router = express.Router();
 router.get('/coffee', coffeeController.getCoffee);
+router.get('/coffeelover', coffeeLoverController);
+router.get('/tea', teaController.getTea);
